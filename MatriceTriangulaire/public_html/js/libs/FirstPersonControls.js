16673959397eee8167c79a0701a2891031a11ba8
Sébastien Menanteau <menanteau.sebastien@icloud.com>
2013-04-08 16:28:56 +0200

    CSS & ajout mouvements haut, bas, gauche, droite

--- MatriceTriangulaire/public_html/js/libs/FirstPersonControls.js
+++ MatriceTriangulaire/public_html/js/libs/FirstPersonControls.js
@@ -142,22 +142,17 @@ THREE.FirstPersonControls = function ( object, domElement ) {
 
 		switch ( event.keyCode ) {
 
-			case 38: /*up*/
-			case 87: /*W*/ this.moveForward = true; break;
 
-			case 37: /*left*/
-			case 65: /*A*/ this.moveLeft = true; break;
-
-			case 40: /*down*/
-			case 83: /*S*/ this.moveBackward = true; break;
-
-			case 39: /*right*/
+                        //case 87: /*W*/ this.moveForward = true; break;
+                        //case 83: /*S*/ this.moveBackward = true; break;
+                        
+                        case 81: /*Q*/ this.moveLeft = true; break;
 			case 68: /*D*/ this.moveRight = true; break;
-
-			case 82: /*R*/ this.moveUp = true; break;
-			case 70: /*F*/ this.moveDown = true; break;
-
-			case 81: /*Q*/ this.freeze = !this.freeze; break;
+                        
+			case 90: /*Z*/ this.moveUp = true; break;
+			case 83: /*S*/ this.moveDown = true; break;
+                        
+			case 70: /*F*/ this.freeze = !this.freeze; break;
 
 		}
 
@@ -166,22 +161,13 @@ THREE.FirstPersonControls = function ( object, domElement ) {
 	this.onKeyUp = function ( event ) {
 
 		switch( event.keyCode ) {
-
-			case 38: /*up*/
-			case 87: /*W*/ this.moveForward = false; break;
-
-			case 37: /*left*/
-			case 65: /*A*/ this.moveLeft = false; break;
-
-			case 40: /*down*/
-			case 83: /*S*/ this.moveBackward = false; break;
-
-			case 39: /*right*/
+                        
+                        case 81: /*Q*/ this.moveLeft = false; break;
 			case 68: /*D*/ this.moveRight = false; break;
-
-			case 82: /*R*/ this.moveUp = false; break;
-			case 70: /*F*/ this.moveDown = false; break;
-
+                        
+			case 90: /*Z*/ this.moveUp = false; break;
+			case 83: /*S*/ this.moveDown = false; break;
+                        
 		}
 
 	};
